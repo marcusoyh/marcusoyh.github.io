f08da9586d80c615517be3b977ea6e8db3b2b2c3
marcusoyh <ongmarcus204@gmail.com>
2021-08-24 00:10:14 +0800

    Updates

---
+++ precache-manifest.9e130f31e7fb020896801250496d7297.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b4bc629d31a4dd6814045008339bfd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2bb075c888f2083b8deb",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "36f3df55f20064c922b6",
+    "url": "./static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "2bb075c888f2083b8deb",
+    "url": "./static/js/2.67e1f4d1.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.67e1f4d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f3df55f20064c922b6",
+    "url": "./static/js/main.1d18577e.chunk.js"
+  },
+  {
+    "revision": "03ecbe1cafddbcb26642",
+    "url": "./static/js/runtime-main.4d8ccb4d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/marcusoyh.github.io/precache-manifest.7579520836dd1000361919697ed6d557.js"
+  "./precache-manifest.9e130f31e7fb020896801250496d7297.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marcusoyh.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
