12b1034b76c64cfec3fe1fb74550a56eb5e0b438
marcusoyh <ongmarcus204@gmail.com>
2021-08-23 20:01:32 +0800

    Updates

---
+++ precache-manifest.2d9fbcfcf6d6bc48335c0ed73e1be1e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc7efd2cd09ac4f8c861ddd3d40a0d9",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "533c49a671ac21411ab4",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "640baf50b7adc6b52112",
+    "url": "/react-frontend-dev-portfolio/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "533c49a671ac21411ab4",
+    "url": "/react-frontend-dev-portfolio/static/js/2.92f34d88.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.92f34d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640baf50b7adc6b52112",
+    "url": "/react-frontend-dev-portfolio/static/js/main.8fab1148.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.f5123b2bb91232354c32b563a661a77b.js"
+  "/react-frontend-dev-portfolio/precache-manifest.2d9fbcfcf6d6bc48335c0ed73e1be1e1.js"
 );
 
 self.addEventListener('message', (event) => {
